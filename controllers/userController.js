1757ec2c794b80ee0b943585e805cc252eb0782c
NakiCode <nakicode07@gmail.com>
2024-01-18 21:19:24 +0200

    fixing data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,14 @@
 import { tbl_User } from "../models/userModel.js";
 import catchAsync from "../helpers/catch/catchAsync.js";
 import checkPasswordStrength from "../helpers/keys/pwd.js";
-import sender  from "../helpers/mail/sender.js";
-import * as typeEmail from "../helpers/mail/emailTypes.js"
+import sender from "../helpers/mail/sender.js";
+import * as typeEmail from "../helpers/mail/emailTypes.js";
+import * as token from "../helpers/jwt/token.js";
+import attachCookies from "../helpers/jwt/cookies.js";
 
+// ####################### CREATE USER 
 export const createUser = catchAsync(async (req, res, next) => {
-    const {fullname, username, email, phone, address, firebaseToken, password, confirmpassword } = req.body
+    const { fullname, username, email, phone, address, firebaseToken, password, confirmpassword } = req.body
     let isMatch = checkPasswordStrength(password, confirmpassword)
     if (!isMatch.success) {
         return res.status(isMatch.statusCode).json({
@@ -14,7 +17,7 @@ export const createUser = catchAsync(async (req, res, next) => {
             message: isMatch.message
         })
     }
-    const user = await tbl_User.create({fullname, username, email, phone, address, password, firebaseToken})
+    const user = await tbl_User.create({ fullname, username, email, phone, address, password, firebaseToken })
     res.status(201).json({
         success: true,
         statusCode: 201,
@@ -26,7 +29,126 @@ export const createUser = catchAsync(async (req, res, next) => {
     format.code = user.code
     await sender(user.email, format)
     // send sms
+})
+// ########################################################################
+export const verifyUser = catchAsync(async (req, res, next) => {
+    const code = req.query.code;
+    const user = await tbl_User.findOne({ code: code }).select(
+        "+codeExpiresAt +tokenId +code"
+    );
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            statusCode: 404,
+            data: [],
+            message: "User not found",
+        });
+    }
+    if (user.isExpires(new Date(), user.codeExpiresAt)) {
+        return res.status(400).json({
+            success: false,
+            statusCode: 400,
+            data: [],
+            message: "Code expired",
+        });
+    }
+    user.codeExpiresAt = null;
 
+    if (req.path === "/verifyEmail") {
+        user.isEmail = true;
+    } else if (req.path === "/verifyPhone") {
+        user.isPhone = true;
+    }
+    user.code = null;
+    user.save({ new: true, runValidators: true });
 
-    // send push
+    let tokenData = token.attachTokenToUser(user);
+    attachCookies(tokenData.access, tokenData.refresh, res);
+
+    res.status(200).json({
+        success: true,
+        statusCode: 200,
+        data: user,
+        ...tokenData,
+        message: "User verified successfully",
+    });
+});
+
+export const verifyCode = catchAsync(async (req, res, next) => {
+    const code = req.query.code;
+    const user = await tbl_User.findOne({ code: code });
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            statusCode: 404,
+            data: [],
+            message: "User not found",
+        });
+    }
+    if (user.isExpires(new Date(), user.codeExpiresAt)) {
+        return res.status(400).json({
+            success: false,
+            statusCode: 400,
+            data: [],
+            message: "Code expired",
+        });
+    }
+    user.codeExpiresAt = null;
+    user.save({ new: true, runValidators: true });
+
+    res.status(200).json({
+        success: true,
+        statusCode: 200,
+        data: user,
+        message: "User verified successfully",
+    });
+});
+
+// #################### LOGIN #############################################
+export const login = catchAsync(async (req, res, next) => {
+    const { email, username, phone, password } = req.body
+    const user = await tbl_User.findOne({ $or: [{ email }, { username }, { phone }] });
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            statusCode: 404,
+            data: [],
+            message: "User not found",
+        });
+    }
+    if (!user.isEmail) {
+        return res.status(400).json({
+            success: false,
+            statusCode: 400,
+            data: [],
+            message: "Please verify your email first",
+        });
+    }
+    if (!user.isPhone) {
+        return res.status(400).json({
+            success: false,
+            statusCode: 400,
+            data: [],
+            message: "Please verify your phone first",
+        });
+    }
+    if (!await user.checkMatchPassword(password)) {
+        return res.status(400).json({
+            success: false,
+            statusCode: 400,
+            data: [],
+            message: "Incorrect password",
+        });
+    }
+
+    let tokenData = token.attachTokenToUser(user);
+    attachCookies(tokenData.access, tokenData.refresh, res);
+
+    res.status(200).json({
+        success: true,
+        statusCode: 200,
+        data: user,
+        ...tokenData,
+        message: "User logged in successfully",
+    });
 })
--- models/UserModel.js
+++ models/UserModel.js
@@ -5,138 +5,125 @@ import generateCode from "../helpers/code/codeGenerate.js";
 import * as date from "../helpers/date/time.js";
 
 const errorMessages = {
-    required: "The {PATH} is required",
-    minLength: "The {PATH} must be at least {MINLENGTH} characters",
-    unique: "This {PATH} already exists",
-    emailFormat: "The email format is not valid",
-    phoneFormat: "The phone number format is not valid",
-    passwordFormat: "The password format is not valid",
+  required: "The {PATH} is required",
+  minLength: "The {PATH} must be at least {MINLENGTH} characters",
+  unique: "This {PATH} already exists",
+  emailFormat: "The email format is not valid",
+  phoneFormat: "The phone number format is not valid",
+  passwordFormat: "The password format is not valid",
 };
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     fullname: {
-        type: String,
-        trim: true,
-        required: [true, errorMessages.required],
-        minLength: [6, errorMessages.minLength.replace("{MINLENGTH}", "6")],
+      type: String,
+      trim: true,
+      required: [true, errorMessages.required],
+      minlength: [6, errorMessages.minLength.replace("{MINLENGTH}", "6")]
     },
     username: {
-        type: String,
-        trim: true,
-        required: [true, errorMessages.required],
-        unique: [true, errorMessages.unique],
-        minLength: [6, errorMessages.minLength.replace("{MINLENGTH}", "6")],
+      type: String,
+      trim: true,
+      required: [true, errorMessages.required],
+      unique: [true, errorMessages.unique],
+      minlength: [6, errorMessages.minLength.replace("{MINLENGTH}", "6")]
     },
     email: {
-        type: String,
-        trim: true,
-        unique: [true, errorMessages.unique],
-        required: [true, errorMessages.required],
-        validate: [validator.isEmail, errorMessages.emailFormat],
+      type: String,
+      trim: true,
+      unique: [true, errorMessages.unique],
+      required: [true, errorMessages.required],
+      validate: [validator.isEmail, errorMessages.emailFormat]
     },
     phone: {
-        type: String,
-        trim: true,
-        unique: [true, errorMessages.unique],
-        required: [true, errorMessages.required],
-        validate: [validator.isMobilePhone, errorMessages.phoneFormat],
+      type: String,
+      trim: true,
+      unique: [true, errorMessages.unique],
+      required: [true, errorMessages.required],
+      validate: [validator.isMobilePhone, errorMessages.phoneFormat]
     },
     isPhone: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     isEmail: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     address: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true
     },
     password: {
-        type: String,
-        trim: true,
-        minLength: [6, errorMessages.minLength.replace("{MINLENGTH}", "6")],
-        required: [true, errorMessages.required],
-        select: false
+      type: String,
+      trim: true,
+      minlength: [6, errorMessages.minLength.replace("{MINLENGTH}", "6")],
+      required: [true, errorMessages.required],
+      select: false
     },
     code: {
-        type: String,
-        trim: true,
-        default: generateCode(6),
-        select: false
+      type: String,
+      trim: true,
+      default: generateCode(6),
+      select: false
     },
     codeExpiresAt: {
-        type: Date,
-        default: date.DefaultDateExpires(),
-        select: false
+      type: Date,
+      default: date.DefaultDateExpires(),
+      select: false
     },
     firebaseToken: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true
     },
     tokenId: {
-        type: String,
-        default: generateCode(8),
-        select: false
+      type: String,
+      default: generateCode(8),
+      select: false
     },
     profil: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true
     },
     couverture: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true
     },
     role: {
-        type: String,
-        trim: true,
-        uppercase: true,
-        enum: ["ADMIN", "USER", "ANONYMOUS"],
-        default: "USER"
-    }
-}, { timestamps: true });
+      type: String,
+      trim: true,
+      uppercase: true,
+      enum: ["ADMIN", "USER", "ANONYMOUS"],
+      default: "USER"
+    },
+  },
+  { timestamps: true }
+);
 
-// Vérifie si le mot de passe correspond
-userSchema.methods.checkMatchPassword = async (candidatePassword, password) => {
-    try {
-        return await bcrypt.compare(candidatePassword, password);
-    } catch (error) {
-        // Gérer l'erreur ici
-        throw error;
-    }
+// Check if the password matches
+userSchema.methods.checkMatchPassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw error;
+  }
 };
-// User generate token, refresh token and cookies
-userSchema.methods.generateToken = async function () {
-    try {
-        const token = await jwt.sign(
-            { _id: this._id,
-            type:"access", 
-            role: this.role, 
-            tokenId: this.tokenId 
-            }, process.env.JWT_SECRET, 
-            {expiresIn: process.env.JWT_EXPIRES_IN}
-        );
-        return token;
-    } catch (error) {
-        throw error;
-    }
+
+// Check if the code is valid
+userSchema.methods.isExpires = function (currentDateTime, givenDateTime) {
+  return date.isDateTimeExpires(currentDateTime, givenDateTime);
 };
-// Vérifie si le code est valide
-userSchema.methods.isExpires = (currentDateTime, givenDateTime) => {
-    let expires = date.isDateTimeExpires(currentDateTime, givenDateTime);
-    return expires;
-}
-// Hash le mot de passe avant de le sauvegarder
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+
+// Hash the password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 export const tbl_User = mongoose.model("tbl_User", userSchema);
