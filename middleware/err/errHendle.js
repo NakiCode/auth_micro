e8f467d9ef986bfe7afbc951d0e11d0f6e819e81
NakiCode <nakicode07@gmail.com>
2024-01-21 00:06:08 +0200

    check userRole Success fix

--- middleware/err/errHendle.js
+++ middleware/err/errHendle.js
@@ -1,133 +1,88 @@
 import mongoose from "mongoose";
 
 const response = (code, success, data, message) => {
-  const responseData = {
-    statusCode: code,
-    success: success,
-    data: data ? data : [],
-    message: message,
-  };
-  return responseData;
+    const responseData = {
+        statusCode: code,
+        success: success,
+        data: data ? data : [],
+        message: message,
+    };
+    return responseData;
 };
-
 const errorHandle = (err, req, res, next) => {
-  console.log(err);
-
-  if (err.name === "ValidationError") {
-    const errors = Object.values(err.errors).map((el) => el.message);
-    const message = errors.join('. ');
-    const respo = response(400, false, [], message);
-    return res.status(400).json(respo);
-  }
-
-  if (err.name === "CorsError") {
-    const message = err.message;
-    const respo = response(403, false, [], message);
-    return res.status(403).json(respo);
-  }
-
-  if (err instanceof mongoose.CastError) {
-    const respo = response(400, false, [], "Invalid ID");
-    return res.status(400).json(respo);
-  }
-
-  if (
-    err instanceof SyntaxError &&
-    err.status === 400 &&
-    "body" in err
-  ) {
-    const respo = response(400, false, [], "Requête invalide");
-    return res.status(400).json(respo);
-  }
-
-  if (err.name === "ReferenceError") {
-    const respo = response(400, false, [], "Requête invalide");
-    return res.status(400).json(respo);
-  }
-
-  if (err.name === "DuplicateFieldTrack") {
-    const respo = response(409, false, [], err.message);
-    console.log(err);
-    return res.status(409).json(respo);
-  }
-
-  if (err.name === "FileErrHandle") {
-    const respo = response(400, false, [], err.message);
-    return res.status(400).json(respo);
-  }
-
-  if (err.name === "MulterError") {
-    const respo = response(400, false, [], err.message);
-    return res.status(400).json(respo);
-  }
-
-  if (err.name === "TypeError") {
-    const respo = response(400, false, [], err.message);
-    return res.status(400).json(respo);
-  }
-
-  if (err.name === "NotFindErr") {
-    const respo = response(404, false, [], err.message);
-    return res.status(404).json(respo);
-  }
-
-  if (err.code === "ENOENT") {
-    const respo = response(400, false, [], "Problème d'accès au fichier");
-    return res.status(400).json(respo);
-  }
-
-  if (err.name === "MongoServerSelectionError") {
-    const respo = response(
-      500,
-      false,
-      [],
-      "Impossible de se connecter à la base de données MongoDB"
-    );
-    return res.status(500).json(respo);
-  }
-
-  if (err.name === "MongoServerError" && err.code === 11000) {
-    const value = Object.keys(err.keyValue)[0];
-    const respo = response(
-      409,
-      false,
-      [],
-      `Un(e) ${value} est invalide ou existe déjà. Veuillez choisir un autre`
-    );
-    return res.status(409).json(respo);
-  }
-
-  if (err.name === "JsonWebTokenError") {
-    const respo = response(
-      403,
-      false,
-      [],
-      "Vous n'avez pas d'autorisation d'accéder à cette ressource"
-    );
-    return res.status(401).json(respo);
-  }
-
-  if (err.name === "AssertionError") {
-    const respo = response(
-      403,
-      false,
-      [],
-      "Veuillez réessayer ultérieurement !"
-    );
-    return res.status(403).json(respo);
-  }
-
-  if (err.name === "ValidatorError") {
-    const respo = response(400, false, [], "Requête invalide");
-    return res.status(400).json(respo);
-  }
-
-  if (err instanceof Error) {
-    const respo = response(500, false, [], "Un problème est survenu");
-    return res.status(500).json(respo);
-  }
-
-  return next(err);
+    if (err.name === "ValidationError") {
+        const errors = Object.values(err.errors).map((el) => el.message);
+        const message = errors.join('. ');
+        const respo = response(400, false, [], message);
+        return res.status(400).json(respo);
+    }
+    if (err.name === "CorsError") {
+        const message = err.message;
+        const respo = response(403, false, [], message);
+        return res.status(403).json(respo);
+    }
+    if (err instanceof mongoose.CastError) {
+        const respo = response(400, false, [], "Invalid ID");
+        return res.status(400).json(respo);
+    }
+    if (
+        err instanceof SyntaxError &&
+        err.status === 400 &&
+        "body" in err
+    ) {
+        const respo = response(400, false, [], "Requête invalide");
+        return res.status(400).json(respo);
+    }
+    if (err.name === "ReferenceError") {
+        const respo = response(400, false, [], "Requête invalide");
+        return res.status(400).json(respo);
+    }
+    if (err.name === "FileErrHandle") {
+        const respo = response(400, false, [], err.message);
+        return res.status(400).json(respo);
+    }
+    if (err.name === "MulterError") {
+        const respo = response(400, false, [], err.message);
+        return res.status(400).json(respo);
+    }
+    if (err.name === "TypeError") {
+        const respo = response(400, false, [], err.message);
+        return res.status(400).json(respo);
+    }
+    if (err.name === "NotFindErr") {
+        const respo = response(404, false, [], err.message);
+        return res.status(404).json(respo);
+    }
+    if (err.code === "ENOENT") {
+        const respo = response(400, false, [], "Problème d'accès au fichier");
+        return res.status(400).json(respo);
+    }
+    if (err.name === "MongoServerSelectionError") {
+        const respo = response(500, false, [], "Impossible de se connecter à la base de données MongoDB");
+        return res.status(500).json(respo);
+    }
+    if (err.name === "MongoServerError" && err.code === 11000) {
+        const value = Object.keys(err.keyValue)[0];
+        const respo = response(409, false, [], `Un(e) ${value} est invalide ou existe déjà. Veuillez choisir un autre`);
+        return res.status(409).json(respo);
+    }
+    if (err.name === "JsonWebTokenError") {
+        const respo = response(403, false, [], "Vous n'avez pas d'autorisation d'accéder à cette ressource");
+        return res.status(401).json(respo);
+    }
+    if (err.name === "AssertionError") {
+        const respo = response(403, false, [], "Veuillez réessayer ultérieurement !");
+        return res.status(403).json(respo);
+    }
+    if (err.name === "ValidatorError") {
+        const respo = response(400, false, [], "Requête invalide");
+        return res.status(400).json(respo);
+    }
+    if (err instanceof Error) {
+        const respo = response(500, false, [], "Un problème est survenu");
+        return res.status(500).json(respo);
+    }
+    return next(err);
 };
 
 export default errorHandle;
--- middleware/roles/getUserRoles.js
+++ middleware/roles/getUserRoles.js
@@ -1,39 +1,49 @@
-import catchAsync from "../../helpers/err/catchAsync.js";
-import { tbl_Restaurent } from "../../models/restaurent/restoModel.js";
-import { tbl_User } from "../../models/user/userModel.js";
-import response from "../../helpers/response/responseMessage.js";
-const findUserMiddleware = (roles) => catchAsync(async (req, res, next) => {
-    const allRoles = {
-        ADMIN: "ADMIN",
-        USER: "USER",
-        ANONYMOUS: "ANONYMOUS"
+import catchAsync from "../../middleware/catch/catchAsync.js";
+import { tbl_User } from "../../models/UserModel.js";
+
+const isAuthorizeToAccess = (roles) => catchAsync(async (req, res, next) => {
+  const allRoles = {
+    ADMIN: "ADMIN",
+    USER: "USER",
+    ANONYMOUS: "ANONYMOUS"
+  };
+
+  const validRoles = roles.filter(role => Object.values(allRoles).includes(role));
+
+  if (validRoles.length === 0) {
+    const respo = {
+      statusCode: 400,
+      success: false,
+      data: [],
+      message: "Veuillez renseigner le rôle"
     };
-    const validRoles = roles.filter(role => Object.values(allRoles).includes(role));
-    if (validRoles.length === 0) {
-        let respo = response(400, false, [], "Veuillez renseigner le role");
-        return res.status(400).json(respo);
-    }
-    const user = await tbl_User.findOne({ _id: req.user._id }).select("role");
-    if (!user) {
-        let respo = response(404, false, [], "Verifier vos informations d'authentification");
-        return res.status(404).json(respo);
-    }
-    if (!validRoles.includes(user.role)) {
-        let respo = response(401, false, [], "Veuillez vous connecter ou recommencer ultèrement");
-        return res.status(401).json(respo);
-    }
-    if (user.role === "ADMIN") {
-        const restaurent = await tbl_Restaurent.findOne({ user: user._id }).select("_id");
-        if (!restaurent) {
-            let respo = response(401, false, [], "Vous n'etes pas autorisé à éffectuer cette operation !!");
-            return res.status(401).json(respo);
-        }
-        req.userRole = user.role;
-        req.id_restaurent = restaurent._id;
-        return next();
-    }
-    req.userRole = user.role;
-    return next();
+    return res.status(400).json(respo);
+  }
+
+  const user = await tbl_User.findOne({ _id: req.user._id }).select("role");
+
+  if (!user) {
+    const respo = {
+      statusCode: 404,
+      success: false,
+      data: [],
+      message: "Veuillez vous connecter ou réessayer ultérieurement"
+    };
+    return res.status(404).json(respo);
+  }
+
+  if (!validRoles.includes(user.role)) {
+    const respo = {
+      statusCode: 401,
+      success: false,
+      data: [],
+      message: "Vous n'avez pas l'autorisation d'accéder à cette ressource"
+    };
+    return res.status(401).json(respo);
+  }
+
+  req.userRole = user.role;
+  return next();
 });
 
-export default findUserMiddleware;
+export default isAuthorizeToAccess;
