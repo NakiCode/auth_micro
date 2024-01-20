fe9566901e2ceb022cd9d41b8f3f1dec61b47f86
NakiCode <nakicode07@gmail.com>
2024-01-20 23:45:03 +0200

    middleware fix successfully

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ import moment from "moment-timezone";
 import dbConnect from "./config/db.js";
 import morgan from "morgan";
 import fs from 'fs';
-import cookieParser  from "cookie-parser";
-// APP CONFIG
-const app = express();
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import errorHandle from "./middleware/err/errHendle.js";
 
 dotenv.config();
 moment.tz.setDefault(process.env.TIMEZONE);
-// -------------------------------------------------------
+
+const app = express();
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("files"));
-// COOKIES PARSER
 app.use(cookieParser());
-// -------------------------------------------------------
-// MORGAN
+
 if (process.env.NODE_ENV === "production") {
     const accessLogStream = fs.createWriteStream('access.log', { flags: 'a' });
     app.use(morgan('combined', { stream: accessLogStream }));
@@ -26,20 +30,50 @@ if (process.env.NODE_ENV === "production") {
     app.use(morgan("combined"));
 }
 
+app.use(helmet());
+app.use(helmet.frameguard({ action: "sameorigin" }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(mongoSanitize());
+app.use(express.urlencoded({ extended: true, limit: "3kb" }));
 
+app.use(express.static("Fichiers/"));
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
+app.enable("trust proxy");
 
-// ROUTES
+if (process.env.NODE_ENV === "development") {
+    app.use(cors());
+} else if (process.env.NODE_ENV === "production") {
+    const whitelist = ["192.168.40.58"];
+    const corsOptions = {
+        origin: function (origin, callback) {
+            if (whitelist.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Access denied by CORS"));
+            }
+        },
+    };
+    app.use(cors(corsOptions));
+}
 
+app.use(morgan("dev"));
+app.use(hpp());
 
-// MIDDLEWARE ERROR HANDLE
+// Routes
 
-// -------------------------------------------------------
-const PORT = process.env.PORT
-// DATABASE
-dbConnect()
-// -------------------------------------------------------
+// Middleware de gestion des erreurs
+app.use(errorHandle);
+
+const PORT = process.env.PORT;
+
+// Connexion à la base de données
+dbConnect();
+
 app.listen(PORT, () => {
-    console.log(`LE  PORT ECOUTE SUR LE PORT : ${PORT}`);
-})
+    console.log(`Le serveur écoute sur le port : ${PORT}`);
+});
--- middleware/err/errHendle.js
+++ middleware/err/errHendle.js
@@ -1,100 +1,133 @@
 import mongoose from "mongoose";
 
 const response = (code, success, data, message) => {
-    const responseData = {
-        statusCode: code,
-        success: success,
-        data: data?data:[],
-        message: message
-    };
-    return responseData;
-}
-const errorHandle = (err, req, res, next)=> {
-    console.log(err)
-     // Gérer les erreurs de validation
-    if (err.name === 'ValidationError') {
-        const errors = Object.values(err.errors).map(el => el.message);
-        const message = errors.join('. ');
-        let respo = response(400, false, [], message);
-        return res.status(400).json(respo);
-        }
-    if (err.name === 'CorsError') {
-        let message = err.message;
-        let respo = response(403, false, [], message);
-        return res.status(403).json(respo);
-        }
-        // Gérer l'erreur de type CastError
-    if (err instanceof mongoose.CastError) {
-        let respo = response(400, false, [], "Invalid ID");
-        return res.status(400).json(respo);
-        }
-        // Gérer les erreurs de SyntaxError
-    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-        let respo = response(400, false, [], 'Requête invalide');
-        return res.status(400).json(respo);
-        }
-        // Gérer les erreurs de references
-    if (err.name === 'ReferenceError'){
-        let respo = response(400, false, [], 'Requête invalide');
-        return res.status(400).json(respo);
-        }
-        // Gérer les erreurs DuplicateFieldTrack
-    if (err.name === "DuplicateFieldTrack") {
-        let respo = response(409, false, [], err.message);
-        console.log(err)
-        return res.status(409).json(respo);
-        }
-        // Gérer les erreurs FileErrHandle
-    if (err.name === "FileErrHandle") {
-        let respo = response(400, false, [], err.message);
-        return res.status(400).json(respo);
-        }
-    if (err.name === 'MulterError'){
-        let respo = response(400, false, [], err.message);
-        return res.status(400).json(respo);
-    }
-    if(err.name === 'TypeError') {
-        let respo = response(400, false, [], err.message);
-        return res.status(400).json(respo);
-    }
-    if(err.name === 'NotFindErr') {
-        let respo = response(404, false, [], err.message);
-        return res.status(404).json(respo);
-    }
-    // 
-    if  (err.code === 'ENOENT') {
-        let respo = response(400, false, [], "Problème d'accès au fichier")
-        return res.status(400).json(respo);
-    }
-    // Gérer les erreurs de connexion
-    // Gérer l'erreur de connexion à MongoDB
-    if (err.name === 'MongoServerSelectionError') {
-        let respo = response(500, false, [], "Impossible de se connecter à la base de données MongoDB");
-        return res.status(500).json(respo);
-    }
-    // Gérer les erreurs MongoServerError
-    if (err.name === 'MongoServerError' && err.code === 11000) {
-        const value = `${Object.keys(err.keyValue)[0]}`
-        let respo = response(409, false, [], `Un(e) ${value} est invalide ou exite déjà. Veuillez choisir un autre`);
-        return res.status(409).json(respo);
-    };
-    if (err.name === "JsonWebTokenError") {
-        let respo = response(403, false, [], "Vous n'avez pas d'autorisation d'accéder à cette resource");
-        return res.status(401).json(respo);
-    }
-    if (err.name === "AssertionError") {
-        let respo = response(403, false, [], "Veuillez recommancez ultérieurement !");
-    }
-    if (err.name === "ValidatorError") {
-        let respo = response(400, false, [], "Requête invalide");
-        return res.status(400).json(respo);
-    }
-    if (err instanceof Error){
-        let respo = response(500, false, [], "Un problème est survenue");
-        return res.status(500).json(respo);
-    };
-    
-    return next(err);
+  const responseData = {
+    statusCode: code,
+    success: success,
+    data: data ? data : [],
+    message: message,
+  };
+  return responseData;
+};
+
+const errorHandle = (err, req, res, next) => {
+  console.log(err);
+
+  if (err.name === "ValidationError") {
+    const errors = Object.values(err.errors).map((el) => el.message);
+    const message = errors.join('. ');
+    const respo = response(400, false, [], message);
+    return res.status(400).json(respo);
+  }
+
+  if (err.name === "CorsError") {
+    const message = err.message;
+    const respo = response(403, false, [], message);
+    return res.status(403).json(respo);
+  }
+
+  if (err instanceof mongoose.CastError) {
+    const respo = response(400, false, [], "Invalid ID");
+    return res.status(400).json(respo);
+  }
+
+  if (
+    err instanceof SyntaxError &&
+    err.status === 400 &&
+    "body" in err
+  ) {
+    const respo = response(400, false, [], "Requête invalide");
+    return res.status(400).json(respo);
+  }
+
+  if (err.name === "ReferenceError") {
+    const respo = response(400, false, [], "Requête invalide");
+    return res.status(400).json(respo);
+  }
+
+  if (err.name === "DuplicateFieldTrack") {
+    const respo = response(409, false, [], err.message);
+    console.log(err);
+    return res.status(409).json(respo);
+  }
+
+  if (err.name === "FileErrHandle") {
+    const respo = response(400, false, [], err.message);
+    return res.status(400).json(respo);
+  }
+
+  if (err.name === "MulterError") {
+    const respo = response(400, false, [], err.message);
+    return res.status(400).json(respo);
+  }
+
+  if (err.name === "TypeError") {
+    const respo = response(400, false, [], err.message);
+    return res.status(400).json(respo);
+  }
+
+  if (err.name === "NotFindErr") {
+    const respo = response(404, false, [], err.message);
+    return res.status(404).json(respo);
+  }
+
+  if (err.code === "ENOENT") {
+    const respo = response(400, false, [], "Problème d'accès au fichier");
+    return res.status(400).json(respo);
+  }
+
+  if (err.name === "MongoServerSelectionError") {
+    const respo = response(
+      500,
+      false,
+      [],
+      "Impossible de se connecter à la base de données MongoDB"
+    );
+    return res.status(500).json(respo);
+  }
+
+  if (err.name === "MongoServerError" && err.code === 11000) {
+    const value = Object.keys(err.keyValue)[0];
+    const respo = response(
+      409,
+      false,
+      [],
+      `Un(e) ${value} est invalide ou existe déjà. Veuillez choisir un autre`
+    );
+    return res.status(409).json(respo);
+  }
+
+  if (err.name === "JsonWebTokenError") {
+    const respo = response(
+      403,
+      false,
+      [],
+      "Vous n'avez pas d'autorisation d'accéder à cette ressource"
+    );
+    return res.status(401).json(respo);
+  }
+
+  if (err.name === "AssertionError") {
+    const respo = response(
+      403,
+      false,
+      [],
+      "Veuillez réessayer ultérieurement !"
+    );
+    return res.status(403).json(respo);
+  }
+
+  if (err.name === "ValidatorError") {
+    const respo = response(400, false, [], "Requête invalide");
+    return res.status(400).json(respo);
+  }
+
+  if (err instanceof Error) {
+    const respo = response(500, false, [], "Un problème est survenu");
+    return res.status(500).json(respo);
+  }
+
+  return next(err);
 };
 
 export default errorHandle;
