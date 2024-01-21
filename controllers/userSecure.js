13259b2a9e0e602898810faac19d0ceb125d26b4
NakiCode <nakicode07@gmail.com>
2024-01-21 14:49:33 +0200

    verify phone account and other successfully implemente

--- controllers/userSecure.js
+++ controllers/userSecure.js
@@ -38,3 +38,41 @@ export const checkEmailCode = catchAsync(async (req, res, next) => {
     const response = { statusCode: 200, success: true, data: attach, message: "Connexion réussie" };
     return res.status(200).json(response);
 });
+
+export const checkPhoneCode = catchAsync(async (req, res, next) => {
+    const defaultResponse = { statusCode: 404, success: false, data: [], message: "Code invalide" };
+    let foundUser = undefined;
+
+    if (!req.query.code || req.query.code.length < 4) {
+        defaultResponse.message = "Veuillez renseigner un code valide";
+        return res.status(404).json(defaultResponse);
+    }
+    if (req.user && req.user._id) {
+        foundUser = await tbl_User.findOne({ $and: [{ _id: req.user._id }, { phoneCode: req.query.code }] }).select('+tokenId');
+    } else {
+        foundUser = await tbl_User.findOne({ phoneCode: req.query.code }).select('+tokenId');
+    }
+    if (!foundUser) {
+        defaultResponse.message = "Code invalide";
+        return res.status(404).json(defaultResponse);
+    }
+    if (foundUser.isExpires('phoneCodeExpiresAt', new Date())) {
+        defaultResponse.message = "Code expire";
+        return res.status(404).json(defaultResponse);
+    }
+
+    foundUser.phoneCode = null;
+    foundUser.phoneCodeExpiresAt = null;
+
+    if (req.path.includes('verify/phone/account')) {
+        foundUser.isPhoneVerified = true;
+    }
+
+    await foundUser.save({ new: true, runValidators: true });
+
+    const attach = jwtToken.attachTokensToUser(foundUser);
+    jwtCookie.attachCookies(attach.access, attach.refresh, res);
+    const response = { statusCode: 200, success: true, data: attach, message: "Connexion réussie" };
+    return res.status(200).json(response);
+
+});
