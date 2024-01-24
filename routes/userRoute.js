ba3128ef988d105187187813eeb6b04dd97fcb09
NakiCode <nakicode07@gmail.com>
2024-01-24 16:41:40 +0200

    user routes fixing

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,11 +9,16 @@ import express from "express";
 
 const router = express.Router();
 
+// CONTROLLERS
 router.post("/signup", userController.createUser);
-
+router.post("/login", userController.login);
+router.patch("/update", protect, userController.updateUser);
+router.get("/find", protect, userController.findUser);
+router.delete("/delete", protect, userController.deleteUser);
+// VERIFY EMAIL OR PHONE ACCOUNT
 router.get("/verify/email/account", userSecure.checkEmailCode);
 router.get("/verify/phone/account", userSecure.checkPhoneCode);
-
+// VERIFY CODE SENT TO EMAIL OR PHONE
 router.get("/verify/code/email", userSecure.checkEmailCode);
 router.get("/verify/code/phone", userSecure.checkPhoneCode);
 // SECURE
@@ -21,9 +26,8 @@ router.patch("/add/phonenumber", protect, userSecure.addPhoneNumber);
 router.patch("/add/email", protect, userSecure.addEmail);
 router.get("/forget/pwd", userSecure.forgetPwd);
 router.patch("/reset/pwd", userSecure.resetPwd);
-
-
-
-
+router.get("/refresh", refresh);
+// UPLOAD IMAGE
+router.post("/upload/image", [protect, uploadImage], dispatchImage);
 
 export default router;
