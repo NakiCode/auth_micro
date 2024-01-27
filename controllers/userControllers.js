42d480c508cc714b38d48d1212d5aace0c78b661
NakiCode <nakicode07@gmail.com>
2024-01-27 12:58:16 +0200

    fix

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -11,24 +11,10 @@ export const createUser = catchAsync(async (req, res, next) => {
     const { fullname, username, email, password, confirmpassword, firebaseToken, address, location } = req.body
     const isStrong = isStrengthPwd(password, confirmpassword);
     if (!isStrong.success) {
-        return res.status(isStrong.statusCode).json({
-            statusCode: isStrong.statusCode,
-            success: isStrong.success,
-            data: [],
-            message: isStrong.message
-        });
+        return res.status(isStrong.statusCode).json({ statusCode: isStrong.statusCode, success: isStrong.success, data: [], message: isStrong.message});
     }
-    const user = await tbl_User.create({
-        fullname, username, email,
-        password,
-        firebaseToken, address, location
-    });
-    res.status(201).json({
-        statusCode: 201,
-        success: true,
-        data: user._id,
-        message: "Compte crée avec succes. Un mail de varification est envoyé sur votre boite mail !"
-    });
+    const user = await tbl_User.create({ fullname, username, email, password, firebaseToken, address, location});
+    res.status(201).json({ statusCode: 201, success: true, data: {_id:user._id}, message: "Compte crée avec succes. Un mail de varification est envoyé sur votre boite mail !"});
     // send email
     let format = emailTypes.createUserAccount
     format.code = user.emailCode
